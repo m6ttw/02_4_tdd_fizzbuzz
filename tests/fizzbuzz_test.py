957d53cd459118a5955059e9ee551bfd3811f2fe
m6ttw <matthewrhyswilliams@live.com>
2020-11-26 11:39:53 +0000

    first passing test

---
+++ tests/fizzbuzz_test.py
@@ -0,0 +1,10 @@
+import unittest
+from src.fizzbuzz import fizzbuzz
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def test_fizzbuzz__3_returns_fizz(self):
+        self.assertEqual("fizz", fizzbuzz(3))
+
+    # def test_fizzbuzz__5_returns_buzz(self):
+    #     self.assertEqual("buzz", fizzbuzz(5))
