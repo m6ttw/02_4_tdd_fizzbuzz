44f0ee741fda79064259740328a19e70ab8bdaa9
m6ttw <matthewrhyswilliams@live.com>
2020-11-26 12:03:36 +0000

    modulus 15 working

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,5 +1,5 @@
 def fizzbuzz(number):
-    if number == 15:
+    if number % 15 == 0:
         return "fizzbuzz"
     elif number % 3 == 0:
         return "fizz"
--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -20,3 +20,8 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_fizzbuzz__4_returns_4_as_string(self):
         self.assertEqual("4", fizzbuzz(4))
+
+    def test_fizzbuzz__15_returns_fizzbuzz(self):
+        self.assertEqual("fizzbuzz", fizzbuzz(45))
+
+    
